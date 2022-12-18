27b96260852fce363b542ea450fafc5094914d2f
VictorEstares <victorestares80@gmail.com>
2022-12-18 23:15:29 +0100

    ya casi

--- APP_API/settings.py
+++ APP_API/settings.py
@@ -27,11 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = []
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-CORS_ORIGIN_ALLOW_ALL=True
-CORS_ALLOW_CREDENTIALS=True 
+
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
